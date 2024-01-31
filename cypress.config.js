78a4518042efe630df80fc119f6013646259ef93
ricardo <ricardosantiagocwb@gmail.com>
2024-01-31 10:41:18 -0300

    Primary test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 	e2e: {
+		defaultCommandTimeout: 20000,
+		baseUrl: 'https://notes-serverless-app.com',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
