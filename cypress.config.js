b59674ff29e06672cdcb06d7a785151b65239d1e
ricardo <ricardosantiagocwb@gmail.com>
2024-02-05 17:22:26 -0300

    Teste com viewport

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,9 @@ module.exports = defineConfig({
 		chromeWebSecurity: false,
 		defaultCommandTimeout: 25000,
 		baseUrl: 'https://notes-serverless-app.com',
-		viewportWidthBreakpoint: 768,
+		env: {
+			viewportWidthBreakpoint: 768,
+		},
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
