e8f4fda4e01d9f4c36b172ef29c6d40439f519d3
ricardo <ricardosantiagocwb@gmail.com>
2024-02-05 11:47:58 -0300

    Alterando o CRUD para Authenticated e adicionando iframe

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 	e2e: {
+		chromeWebSecurity: false,
 		defaultCommandTimeout: 25000,
 		baseUrl: 'https://notes-serverless-app.com',
 		setupNodeEvents(on, config) {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -44,7 +44,6 @@ Cypress.Commands.add('sessionLogin', (
 const attachFileHandler = () => {
   cy.get('#file').selectFile('cypress/fixtures/example.json')
 }
-
 Cypress.Commands.add('createNote', (note, attachFile = false) => {
   cy.visit('/notes/new')
   cy.get('#content').type(note)
@@ -57,7 +56,6 @@ Cypress.Commands.add('createNote', (note, attachFile = false) => {
 
   cy.contains('.list-group-item', note).should('be.visible')
 })
-
 Cypress.Commands.add('editNote', (note, newNoteValue, attachFile = false) => {
   cy.intercept('GET', '**/notes/**').as('getNote')
 
@@ -91,4 +89,24 @@ Cypress.Commands.add('deleteNote', note => {
     .should('not.exist')
 })
 
+// ... Comando Authenticated aqui
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings')
+  cy.get('#storage').type('1')
+  cy.get('#name').type('Mary Doe')
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242')
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271')
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123')
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345')
+  cy.contains('button', 'Purchase').click()
+})
 
