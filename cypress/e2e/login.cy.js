42d6ea8caaf4cd6ed1f397925648f215caa81790
ricardo <ricardosantiagocwb@gmail.com>
2024-02-06 15:35:20 -0300

    Testando correções na identação

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,7 @@
-// cypress/e2e/login.cy.js
+
 describe('Login', () => {
 	it('successfully logs in', () => {
-		cy.guiLogin()
-				 		
+		cy.guiLogin()				 		
 		cy.contains('h1', 'Your Notes').should('be.visible')
 		cy.contains('a', 'Create a new note').should('be.visible')
 	})
